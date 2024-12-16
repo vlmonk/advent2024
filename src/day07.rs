6868c1c4c0cf25e0ee1fbbfe4e0ac5369ac4d625
Eugene Korbut <me@mvl.ru>
2024-12-16 22:59:41 +0500

    7 / 2

--- src/day07.rs
+++ src/day07.rs
@@ -1,4 +1,19 @@
-fn is_match_rest(target: i64, a: i64, b: &[i64]) -> bool {
+fn num_of_digits(mut input: i64) -> i64 {
+    let mut result = 1;
+    while input > 9 {
+        input /= 10;
+        result += 1;
+    }
+
+    result
+}
+
+fn concat(a: i64, b: i64) -> i64 {
+    let d = num_of_digits(b);
+    a * 10_i64.pow(d as u32) + b
+}
+
+fn is_match_rest(target: i64, a: i64, b: &[i64], use_concat: bool) -> bool {
     if b.is_empty() {
         return target == a;
     }
@@ -10,7 +25,16 @@ fn is_match_rest(target: i64, a: i64, b: &[i64]) -> bool {
     let sum = a + b[0];
     let mul = a * b[0];
 
-    is_match_rest(target, sum, &b[1..]) || is_match_rest(target, mul, &b[1..])
+    if use_concat {
+        let contcat = concat(a, b[0]);
+
+        is_match_rest(target, sum, &b[1..], use_concat)
+            || is_match_rest(target, mul, &b[1..], use_concat)
+            || is_match_rest(target, contcat, &b[1..], use_concat)
+    } else {
+        is_match_rest(target, sum, &b[1..], use_concat)
+            || is_match_rest(target, mul, &b[1..], use_concat)
+    }
 }
 
 #[derive(Debug)]
@@ -31,11 +55,11 @@ impl Equation {
         Self(first, rest)
     }
 
-    pub fn is_match(&self) -> bool {
+    pub fn is_match(&self, use_concat: bool) -> bool {
         match self.1.len() {
             0 => false,
             1 => self.0 == self.1[0],
-            _ => is_match_rest(self.0, self.1[0], &self.1[1..]),
+            _ => is_match_rest(self.0, self.1[0], &self.1[1..], use_concat),
         }
     }
 }
@@ -51,11 +75,19 @@ impl Game {
     }
 
     pub fn solve_a(&self) -> i64 {
-        self.0.iter().filter(|e| e.is_match()).map(|e| e.0).sum()
+        self.0
+            .iter()
+            .filter(|e| e.is_match(false))
+            .map(|e| e.0)
+            .sum()
     }
 
-    pub fn solve_b(&self) -> usize {
-        42
+    pub fn solve_b(&self) -> i64 {
+        self.0
+            .iter()
+            .filter(|e| e.is_match(true))
+            .map(|e| e.0)
+            .sum()
     }
 }
 
@@ -67,4 +99,14 @@ fn main() {
     let b = game.solve_b();
 
     println!("A: {}\nB: {}", a, b);
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_concat() {
+        assert_eq!(1234, concat(12, 34));
+    }
 }
