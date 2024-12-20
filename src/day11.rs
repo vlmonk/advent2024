9315b94b21486fd0e0f1a9164791074f6da689b3
Eugene Korbut <me@mvl.ru>
2024-12-20 17:07:27 +0200

    11 / 2

--- src/day11.rs
+++ src/day11.rs
@@ -1,27 +1,37 @@
-fn digits_n(input: i32) -> u32 {
+use std::collections::HashMap;
+
+const TARGET_A: usize = 25;
+const TARGET_B: usize = 75;
+
+fn digits_n(input: i64) -> u32 {
     match input {
         0 => 1,
         i => i.ilog10() + 1,
     }
 }
 
-fn tick_stone(input: i32) -> [Option<i32>; 2] {
+enum TickResult {
+    Single(i64),
+    Double(i64, i64),
+}
+
+fn tick_stone(input: i64) -> TickResult {
     match (input, digits_n(input)) {
-        (0, _) => [Some(1), None],
+        (0, _) => TickResult::Single(1),
         (i, n) if n % 2 == 0 => {
-            let log = 10_i32.pow(n / 2);
+            let log = 10_i64.pow(n / 2);
             let left = i / log;
             let right = i % log;
 
-            [Some(left), Some(right)]
+            TickResult::Double(left, right)
         }
-        (i, _) => [Some(i * 2024), None],
+        (i, _) => TickResult::Single(i * 2024),
     }
 }
 
 #[derive(Debug)]
 struct Field {
-    stones: Vec<i32>,
+    stones: HashMap<i64, usize>,
 }
 
 impl Field {
@@ -29,33 +39,44 @@ impl Field {
         let stones = input
             .split(' ')
             .map(|num| num.trim().parse().expect("invalid num"))
+            .map(|num| (num, 1))
             .collect();
+
         Self { stones }
     }
 
     fn tick(&mut self) {
-        self.stones = self
-            .stones
-            .iter()
-            .flat_map(|n| tick_stone(*n))
-            .flatten()
-            .collect()
+        let mut new_rack = HashMap::new();
+
+        for (&stone, count) in &self.stones {
+            match tick_stone(stone) {
+                TickResult::Single(n) => *new_rack.entry(n).or_default() += count,
+                TickResult::Double(a, b) => {
+                    *new_rack.entry(a).or_default() += count;
+                    *new_rack.entry(b).or_default() += count;
+                }
+            }
+        }
+
+        self.stones = new_rack;
     }
 
     fn len(&self) -> usize {
-        self.stones.len()
+        self.stones.values().sum()
     }
 }
 
 fn main() {
     let data = std::fs::read_to_string("data/day11.txt").expect("Can't read input");
     let mut field = Field::parse(&data);
-    for _ in 0..25 {
-        field.tick();
-    }
 
+    (0..TARGET_A).for_each(|_| field.tick());
     let a = field.len();
+    (0..(TARGET_B - TARGET_A)).for_each(|_| field.tick());
+    let b = field.len();
+
     println!("A: {}", a);
+    println!("B: {}", b);
 }
 
 #[cfg(test)]
